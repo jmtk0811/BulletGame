728ec5109f145acd56315d420857bc1e1a5bcf75
jmtk0811 <66237332+jmtk0811@users.noreply.github.com>
2020-06-04 16:29:30 +0900

    0604강태규

---
+++ Assets/CatPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPlayer : MonoBehaviour
+{
+    Rigidbody rigid;
+    void Start()
+    {
+        Cat kitty = new cat("kitty");
+        kitty.playSound();
+        kitty.Go();
+
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.upArrow))
+        {
+            Debug.Log("앞으로~");
+            transform.position = new Vector3(
+                fransform.position.x
+                , fransform.position.y
+                , fransform.position.z + 3f * Time.deltaTime
+                );
+        }
+        if (Input.GetKey(KeyCode.upArrow))
+        {
+            Debug.Log("뒤로~");
+            transform.position = new Vector3(
+                fransform.position.x
+                , fransform.position.y
+                , fransform.position.z - 3f * Time.deltaTime);
+        }
+    }
+
+}
